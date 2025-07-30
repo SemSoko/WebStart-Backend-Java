d8f99fb5b48502c8d464619e25000d2e7b2ca92a
SemSoko <info@semensokolski.de>
2025-07-30 12:30:06 +0300

    feat(todo/dto): implement NewTodoRequest for incoming JSON data

--- src/main/java/com/semsoko/webstartbackend/todo/dto/NewTodoRequest.java
+++ src/main/java/com/semsoko/webstartbackend/todo/dto/NewTodoRequest.java
@@ -1,4 +1,19 @@
 package com.semsoko.webstartbackend.todo.dto;
 
+/**
+ * NewTodoRequest ist ein DTO für die Eingabe per JSON.
+ * Es kapselt nur die Felder, die der Client beim Erstellen eines Todos mitschickt.
+ * So trennen wir externe Eingabe von interner Logik (z.B. automatische ID-Vergabe).
+ */
+
 public class NewTodoRequest {
+    private String title;
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
 }
