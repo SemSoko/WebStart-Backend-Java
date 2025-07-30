aacbb326484c533ca60f80e201ac5e1b26646c71
SemSoko <info@semensokolski.de>
2025-07-30 15:44:58 +0300

    refactor(todo/service): rename TodoService to InMemoryTodoService for interface-based DI

--- src/main/java/com/semsoko/webstartbackend/todo/service/InMemoryTodoService.java
+++ src/main/java/com/semsoko/webstartbackend/todo/service/InMemoryTodoService.java
@@ -2,18 +2,21 @@ package com.semsoko.webstartbackend.todo.service;
 
 import com.semsoko.webstartbackend.todo.dto.NewTodoRequest;
 import com.semsoko.webstartbackend.todo.model.Todo;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Profile("dev")
 @Service
-public class TodoService {
+public class InMemoryTodoService implements TodoService{
     /**
      * Temporaerer In-Memory-Speicher, wird spaeter ins Repository ausgelagert.
      */
     private final List<Todo> todos = new ArrayList<>();
 
+    @Override
     public Todo createTodo(NewTodoRequest request){
         Todo newTodo = new Todo(request.getTitle());
         todos.add(newTodo);
