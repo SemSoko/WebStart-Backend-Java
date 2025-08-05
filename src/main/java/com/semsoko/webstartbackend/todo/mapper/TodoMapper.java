516082f7888f7547be3b0b6b170779bd560832b0
SemSoko <info@semensokolski.de>
2025-08-05 14:18:31 +0300

    feat(todo/mapper): define TodoMapper interface for DTO transformation

--- src/main/java/com/semsoko/webstartbackend/todo/mapper/TodoMapper.java
+++ src/main/java/com/semsoko/webstartbackend/todo/mapper/TodoMapper.java
@@ -1,4 +1,14 @@
 package com.semsoko.webstartbackend.todo.mapper;
 
+import com.semsoko.webstartbackend.todo.dto.TodoResponse;
+import com.semsoko.webstartbackend.todo.model.Todo;
+import com.semsoko.webstartbackend.todo.model.TodoEntity;
+
+/**
+ * Mapper-Interface zur Umwandlung von internen Todo-Modellen in API-Response-DTOs.
+ * Trennt die Geschaeftslogik von der Repraesentation der Daten in der API.
+ */
 public interface TodoMapper {
+    TodoResponse mapToResponse(Todo todo);
+    TodoResponse mapToResponse(TodoEntity entity);
 }
