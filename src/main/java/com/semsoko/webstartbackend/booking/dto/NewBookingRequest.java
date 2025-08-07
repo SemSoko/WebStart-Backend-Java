67c91247b7316da2d19fbfa22d9811411872b432
SemSoko <info@semensokolski.de>
2025-08-07 17:00:53 +0300

    feat(booking/dto): add NewBookingRequest for POST endpoint

--- src/main/java/com/semsoko/webstartbackend/booking/dto/NewBookingRequest.java
+++ src/main/java/com/semsoko/webstartbackend/booking/dto/NewBookingRequest.java
@@ -1,4 +1,50 @@
 package com.semsoko.webstartbackend.booking.dto;
 
+/**
+ * Zeitformat: "2025-08-07T14:00:00"
+ */
+import java.time.LocalDateTime;
+
 public class NewBookingRequest {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    private Long userId;
+    private Long resourceId;
+
+    /**
+     * Getter und Setter
+     */
+
+    public LocalDateTime getSTartTime(){
+        return this.startTime;
+    }
+
+    public LocalDateTime getEndTime(){
+        return endTime;
+    }
+
+    public Long getUserId(){
+        return this.userId;
+    }
+
+    public void setStartTime(LocalDateTime startTime){
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime){
+        this.endTime = endTime;
+    }
+
+    public void setUserId(Long userId){
+        this.userId = userId;
+    }
+
+    public Long getResourceId(){
+        return this.resourceId;
+    }
+
+    public void setResourceId(Long resourceId){
+        this.resourceId = resourceId;
+    }
 }
