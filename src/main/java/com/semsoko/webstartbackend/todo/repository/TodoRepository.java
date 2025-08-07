322e7718c75241873017bedad15ff73147b62baf
SemSoko <info@semensokolski.de>
2025-08-07 12:03:57 +0300

    feat(todo/repository): add findById method to TodoRepository

--- src/main/java/com/semsoko/webstartbackend/todo/repository/TodoRepository.java
+++ src/main/java/com/semsoko/webstartbackend/todo/repository/TodoRepository.java
@@ -3,9 +3,11 @@ package com.semsoko.webstartbackend.todo.repository;
 import com.semsoko.webstartbackend.todo.model.Todo;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TodoRepository {
     void save(Todo todo);
     List<Todo> findAll();
     void deleteById(Long id);
+    Optional<Todo> findById(Long id);
 }
