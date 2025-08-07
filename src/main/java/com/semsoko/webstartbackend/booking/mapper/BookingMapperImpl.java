473b0b7a92deef7ac9e5529bfa3cc1b1d41cf092
SemSoko <info@semensokolski.de>
2025-08-07 17:19:07 +0300

    feat(booking/mapper): implement BookinMapper logic in BookingMapperImpl

--- src/main/java/com/semsoko/webstartbackend/booking/mapper/BookingMapperImpl.java
+++ src/main/java/com/semsoko/webstartbackend/booking/mapper/BookingMapperImpl.java
@@ -1,4 +1,35 @@
 package com.semsoko.webstartbackend.booking.mapper;
 
-public class BookingMapperImpl {
+import com.semsoko.webstartbackend.booking.dto.NewBookingRequest;
+import com.semsoko.webstartbackend.booking.dto.BookingResponse;
+import com.semsoko.webstartbackend.booking.model.BookingEntity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookingMapperImpl implements BookingMapper{
+    @Override
+    public BookingEntity toEntity(NewBookingRequest request){
+        BookingEntity entity = new BookingEntity();
+        entity.setStartTime(request.getSTartTime());
+        entity.setEndTime(request.getEndTime());
+        entity.setUserId(request.getUserId());
+        entity.setResourceId(request.getResourceId());
+        entity.setCanceled(false);
+
+        return entity;
+    }
+
+    @Override
+    public BookingResponse toResponse(BookingEntity entity){
+        BookingResponse response = new BookingResponse();
+        response.setId(entity.getId());
+        response.setStartTime(entity.getStartTime());
+        response.setEndTime(entity.getEndTime());
+        response.setUserId(entity.getUserId());
+        response.setResourceId(entity.getResourceId());
+        response.setCanceled(entity.isCanceled());
+
+        return response;
+    }
 }
