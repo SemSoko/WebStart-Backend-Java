8c9f45740c8c10dce9b3a033cadde5a593c26e0d
SemSoko <info@semensokolski.de>
2025-08-07 17:38:32 +0300

    feat(booking/service): define BookingService interface

--- src/main/java/com/semsoko/webstartbackend/booking/service/BookingService.java
+++ src/main/java/com/semsoko/webstartbackend/booking/service/BookingService.java
@@ -1,4 +1,8 @@
 package com.semsoko.webstartbackend.booking.service;
 
+import com.semsoko.webstartbackend.booking.dto.NewBookingRequest;
+import com.semsoko.webstartbackend.booking.dto.BookingResponse;
+
 public interface BookingService {
+    BookingResponse createBooking(NewBookingRequest request);
 }
