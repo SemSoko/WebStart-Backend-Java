1e4b52c3aabcd4b8ccca141aebb1f5e878f6bc76
SemSoko <info@semensokolski.de>
2025-08-07 16:44:37 +0300

    feat(booking): implement BookingController

--- src/main/java/com/semsoko/webstartbackend/booking/controller/BookingController.java
+++ src/main/java/com/semsoko/webstartbackend/booking/controller/BookingController.java
@@ -1,4 +1,25 @@
 package com.semsoko.webstartbackend.booking.controller;
 
+import com.semsoko.webstartbackend.booking.dto.NewBookingRequest;
+import com.semsoko.webstartbackend.booking.dto.BookingResponse;
+import com.semsoko.webstartbackend.booking.service.BookingService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/bookings")
 public class BookingController {
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService){
+       this.bookingService = bookingService;
+    }
+
+    @PostMapping
+    public ResponseEntity<BookingResponse> createBooking(@RequestBody NewBookingRequest request){
+        BookingResponse response = bookingService.createBooking(request);
+
+        return ResponseEntity.ok(response);
+    }
 }
