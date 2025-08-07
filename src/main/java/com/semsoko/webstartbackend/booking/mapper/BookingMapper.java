c63e1796a0b60c2d6f44c03f24f2527cb2e14d76
SemSoko <info@semensokolski.de>
2025-08-07 17:11:57 +0300

    feat(booking/mapper): define BookingMapper interface

--- src/main/java/com/semsoko/webstartbackend/booking/mapper/BookingMapper.java
+++ src/main/java/com/semsoko/webstartbackend/booking/mapper/BookingMapper.java
@@ -1,4 +1,10 @@
 package com.semsoko.webstartbackend.booking.mapper;
 
-public interface BookingMapper {
+import com.semsoko.webstartbackend.booking.dto.NewBookingRequest;
+import com.semsoko.webstartbackend.booking.dto.BookingResponse;
+import com.semsoko.webstartbackend.booking.model.BookingEntity;
+
+public interface BookingMapper{
+    BookingEntity toEntity(NewBookingRequest request);
+    BookingResponse toResponse(BookingEntity entity);
 }
