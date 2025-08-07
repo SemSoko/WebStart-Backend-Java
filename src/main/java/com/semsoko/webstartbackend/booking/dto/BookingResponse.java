df1ea846c798544210727089766b49605f9716e6
SemSoko <info@semensokolski.de>
2025-08-07 17:08:30 +0300

    feat(booking/dto): add BookingResponse for booking POST result

--- src/main/java/com/semsoko/webstartbackend/booking/dto/BookingResponse.java
+++ src/main/java/com/semsoko/webstartbackend/booking/dto/BookingResponse.java
@@ -1,4 +1,67 @@
 package com.semsoko.webstartbackend.booking.dto;
 
-public class BookingResponse {
+import java.time.LocalDateTime;
+
+public class BookingResponse{
+    private Long id;
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    private Long userId;
+    private Long resourceId;
+
+    private boolean canceled;
+
+    /**
+     * Getter und Setter
+     */
+
+    public Long getId(){
+        return id;
+    }
+
+    public LocalDateTime getStartTime(){
+        return this.startTime;
+    }
+
+    public LocalDateTime getEndTime(){
+        return this.endTime;
+    }
+
+    public Long getUserId(){
+        return this.userId;
+    }
+
+    public Long getResourceId(){
+        return this.resourceId;
+    }
+
+    public boolean isCanceled(){
+        return this.canceled;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setStartTime(LocalDateTime startTime){
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime){
+        this.endTime = endTime;
+    }
+
+    public void setUserId(Long userId){
+        this.userId = userId;
+    }
+
+    public void setResourceId(Long resourceId){
+        this.resourceId = resourceId;
+    }
+
+    public void setCanceled(boolean canceled){
+        this.canceled = canceled;
+    }
 }
