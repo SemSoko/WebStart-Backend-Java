72d027339f6d970e95e7a5e63cf2e4057f008491
SemSoko <info@semensokolski.de>
2025-08-14 08:51:44 +0300

    feat(auth/service): add AuthServiceImpl skeleton with injected token services

--- src/main/java/com/semsoko/webstartbackend/auth/service/AuthServiceImpl.java
+++ src/main/java/com/semsoko/webstartbackend/auth/service/AuthServiceImpl.java
@@ -1,4 +1,34 @@
 package com.semsoko.webstartbackend.auth.service;
 
-public class AuthServiceImpl {
+import com.semsoko.webstartbackend.auth.dto.*;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService{
+    private final TokenService tokenService;
+    private final TokenStoreService tokenStoreService;
+
+    public AuthServiceImpl(
+            TokenService tokenService,
+            TokenStoreService tokenStoreService
+    ){
+        this.tokenService = tokenService;
+        this.tokenStoreService = tokenStoreService;
+    }
+
+    @Override
+    public LoginResponse login(LoginRequest request){
+        return null;
+    }
+
+    @Override
+    public RefreshTokenResponse refresh(RefreshTokenRequest request){
+        return null;
+    }
+
+    @Override
+    public void logout(LogoutRequest request, String accessTokenJti){
+        
+    }
 }
