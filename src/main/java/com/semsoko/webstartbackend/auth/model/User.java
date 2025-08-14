6fbf762e65a13b72c18ac210f3aa67df895a1962
SemSoko <info@semensokolski.de>
2025-08-14 10:44:14 +0300

    feat(auth/model): add User model with id, username, hashedPassword and roles

--- src/main/java/com/semsoko/webstartbackend/auth/model/User.java
+++ src/main/java/com/semsoko/webstartbackend/auth/model/User.java
@@ -1,4 +1,54 @@
 package com.semsoko.webstartbackend.auth.model;
 
+import java.util.List;
+
 public class User {
+    private String id;
+    private String username;
+    private String hashedPassword;
+    private List<Role> roles;
+
+    public User(){
+
+    }
+
+    public User(
+            String id,
+            String username,
+            String hashedPassword,
+            List<Role> roles
+    ){
+        this.id = id;
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+        this.roles = roles;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getHashedPassword(){
+        return this.hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword){
+        this.hashedPassword = hashedPassword;
+    }
+
+    public void setRoles(List<Role> roles){
+        this.roles = roles;
+    }
 }
